62d2533724b2d7a05a0a2887438809306c9effe3
pratokko <pratokko3@gmail.com>
2023-07-25 03:20:10 +0300

    Deploy scripts added

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -2,13 +2,34 @@
 
 pragma solidity ^0.8.18;
 
-import {Raffle} from '../src/Raffle.sol';
+import {Raffle} from "../src/Raffle.sol";
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
+    function run() external returns (Raffle) {
+        HelperConfig helperConfig = new HelperConfig();
 
+        (
+            uint256 entranceFee,
+            uint256 interval,
+            address vrfCoordinator,
+            bytes32 gasLane,
+            uint64 subscriptionId,
+            uint32 callbackGasLimit
+        ) = helperConfig.activeNetworkConfig();
 
-    function run () external returns (Raffle) {
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            entranceFee,
+            interval,
+            vrfCoordinator,
+            gasLane,
+            subscriptionId,
+            callbackGasLimit
+        );
 
+        vm.stopBroadcast();
+        return raffle;
     }
 }
